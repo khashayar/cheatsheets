96c829d74d2d4548eac060752de87b66bda05290
Khashayar Hajian <me@khashayar.me>
2016-12-22 14:39:57 +0100

    add mongodb sample queries

---
+++ mongodb/query.js
@@ -0,0 +1,26 @@
+// convert a field's type from string to date
+db.collectionName.find({ updatedAt: { $type: 2 } }).forEach(function(doc) {
+  doc.updatedAt = ISODate(doc.updatedAt);
+  db.collectionName.save(doc);
+});
+
+// rename a field
+db.collectionName.update(
+  { foo: 'bar' }, // condition
+  { $rename: { 'oldName': 'newName' } },
+  { multi: true }
+);
+
+// find duplicate values for a given (`name`) field
+db.collectionName.aggregate(
+  { $group: { _id: '$name', ids: { $addToSet: '$_id' }, count: { $sum: 1 } } },
+  { $match: { _id: { $ne: null }, count: { $gt: 1 } } },
+  { $project: { name: '$_id', _id: 0, ids: 1 } }
+);
+
+// remove a field based on a condition
+db.collectionName.update(
+  { userId: '123456'},
+  { $unset: { userId: '' } },
+  { multi: true }
+);
